89df3fb8c01b6506c2b24bd3814e55b112d51c7a
abhinav3398 <3398abhinav@gmail.com>
2022-01-23 17:47:02 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict the popularity of a song given features like acousticness, danceability, key, loudness, etc.',
+    author='Abhinav Lakhani',
+    license='',
+)
